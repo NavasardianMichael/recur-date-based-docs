38eac218531b69678f290395b95da4bf041cd26f
NavasardianMichael <navasardianmichael@gmail.com>
2024-04-04 16:19:28 +0400

    exclude to filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { Header } from './components/header/main';
 import { Docs } from 'components/docs/main';
 import styles from './app.module.css'
-import { Sandbox } from 'components/sandbox/main';
 
 function App() {
   return (
     <div className={styles.app}>
-      <Sandbox />
-      <div className={styles.content}>
-        <Header />
-        <Docs />
-      </div>
+      <Header />
+      <Docs />
     </div>
   );
 }
